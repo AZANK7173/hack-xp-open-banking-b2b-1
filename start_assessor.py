8a44df9f1ebc7250075f901e28d7f12a48115c8f
fhtsibuya <felipe.sibuya@usp.br>
2022-02-20 18:26:16 -0300

    Adding favicon

--- start_assessor.py
+++ start_assessor.py
@@ -1,11 +1,19 @@
 import os
 import streamlit as st
 
+from PIL import Image
+
 from src.external_messages import ComunicationForAllUsersSt, get_users_name
 from src.plataform import show_assessor_all, show_news
 from src.st_app import AppClientData
 from src.api_communication import XpDataApi
 
+im = Image.open("favicon.png")
+st.set_page_config(
+    page_title="XP insights - Assessor",
+    page_icon=im,
+    layout="wide",
+)
 
 client_id = os.environ.get('HACK_XP_CLIENT_ID')
 client_secret = os.environ.get('HACK_XP_CLIENT_SECRET')
--- start_person.py
+++ start_person.py
@@ -1,9 +1,18 @@
 import os
 import streamlit as st
 
+from PIL import Image
+
 from src.external_messages import ComunicationUserSt
 from src.st_app import AppClientData
 from src.api_communication import XpDataApi
+
+im = Image.open("favicon.png")
+st.set_page_config(
+    page_title="XP insights - Cliente",
+    page_icon=im,
+    layout="wide",
+)
 
 app_state = st.experimental_get_query_params()
 app_state = {k: v[0] if isinstance(v, list) else v for k, v in app_state.items()}
